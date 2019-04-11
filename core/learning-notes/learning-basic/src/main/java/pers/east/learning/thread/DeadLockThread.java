f321a5005a325415ffe40ea19bfc45eac820b8e3
eastFu <492510931@qq.com>
2019-04-11 11:14:08 +0800

    thread lock

--- core/learning-notes/learning-basic/src/main/java/pers/east/learning/thread/DeadLockThread.java
+++ core/learning-notes/learning-basic/src/main/java/pers/east/learning/thread/DeadLockThread.java
@@ -1,5 +1,53 @@
 package pers.east.learning.thread;
 
+/**
+ *  死锁
+ *
+ *  概念：同步中，多个线程使用多把锁之间存在等待的现象。
+ *
+ *  原因分析：
+ *  a.线程1将锁1锁住，线程2将锁2锁住，而线程1要继续执行锁2中的代码，线程2要继续执行锁1中的代码，
+ *    但是此时，两个锁均处于锁死状态。最终导致两线程相互等待，进入无限等待状态。
+ *  b.有同步代码块的嵌套动作。
+ *
+ *  解决方法：
+ *  不要写同步代码块嵌套
+ */
 public class DeadLockThread extends Thread{
-    
+
+    boolean flag;
+
+    public DeadLockThread(boolean flag){
+        this.flag=flag;
+    }
+
+    @Override
+    public void run() {
+        if(flag){
+            System.out.println("if语句锁1");
+            synchronized (Demo.LOCK1){
+                //睡眠造成死锁现象
+                /*try {
+                    sleep(1000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }*/
+                synchronized (Demo.LOCK2){
+                    System.out.println("if 语句锁2");
+                }
+            }
+        }else{
+            try {
+                sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            synchronized (Demo.LOCK1){
+                System.out.println("else 语句锁1");
+                synchronized (Demo.LOCK2){
+                    System.out.println("else 语句锁2");
+                }
+            }
+        }
+    }
 }
