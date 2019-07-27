20dafbaa8060fe3ef0010b5dc517c562da92937c
eastFu <492510931@qq.com>
2019-07-27 16:23:43 +0800

    java8 learning finished

--- core/learning-notes/learning-java8/src/main/java/pers/east/learning/java8/newdata/FormaterDemo.java
+++ core/learning-notes/learning-java8/src/main/java/pers/east/learning/java8/newdata/FormaterDemo.java
@@ -1,5 +1,8 @@
 package pers.east.learning.java8.newdata;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author East.F
  * @ClassName: FormaterDemo
@@ -8,14 +11,22 @@ package pers.east.learning.java8.newdata;
  */
 public class FormaterDemo {
     public static void main(String[] args) {
+        testFormater();
+        testParse();
 
     }
 
     private static void testFormater(){
+        LocalDate localDate =LocalDate.now();
+        System.out.println(localDate.format(DateTimeFormatter.BASIC_ISO_DATE));
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        System.out.println(localDate.format(formatter));
     }
 
     private static void testParse(){
-
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate localDate = LocalDate.parse("2009-04-26", formatter);
+        System.out.println(localDate);
     }
 }
