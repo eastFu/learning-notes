554011eb30ccfdebf090f03b67693c8049522f2b
eastFu <492510931@qq.com>
2019-07-30 19:08:03 +0800

    java thread

--- core/learning-notes/learning-thread/src/main/java/pers/east/learning/thread/DeadLockThread.java
+++ core/learning-notes/learning-thread/src/main/java/pers/east/learning/thread/DeadLockThread.java
@@ -57,10 +57,8 @@ class Demo {
     public static final Object LOCK2 = new Object();
 
     public static void main(String[] args) {
-
         DeadLockThread dlt = new DeadLockThread(true);
         DeadLockThread dlt2 = new DeadLockThread(false);
-
         dlt.start();
         dlt2.start();
     }
