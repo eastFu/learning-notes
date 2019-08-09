8cc287f297138ca2afdbbe185b86b2a3b8609c75
eastFu <492510931@qq.com>
2019-08-09 19:56:50 +0800

    algorithm

--- core/learning-notes/learning-algorithm/src/main/java/pers/east/learning/algorithm/sort/FullPermutation.java
+++ core/learning-notes/learning-algorithm/src/main/java/pers/east/learning/algorithm/sort/FullPermutation.java
@@ -3,6 +3,7 @@ package pers.east.learning.algorithm.sort;
 import java.util.Arrays;
 
 public class FullPermutation {
+
     static void allSort(int[] array,int begin,int end){
 //        Arrays.stream(array).forEach(x -> System.out.print(x));
         if(begin==end){
@@ -21,6 +22,7 @@ public class FullPermutation {
         array[a]=array[b];
         array[b]=tem;
     }
+    
     public static void main(String[] args) {
         int[] array={1,2,3};
         allSort(array, 0, array.length-1);
