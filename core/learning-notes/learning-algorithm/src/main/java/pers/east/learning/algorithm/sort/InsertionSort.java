efd2239452a89f9c59f3567f522954a0e13f5614
eastFu <492510931@qq.com>
2019-09-01 20:34:40 +0800

    algorithm： insertion sort

--- core/learning-notes/learning-algorithm/src/main/java/pers/east/learning/algorithm/sort/InsertionSort.java
+++ core/learning-notes/learning-algorithm/src/main/java/pers/east/learning/algorithm/sort/InsertionSort.java
@@ -1,7 +1,26 @@
 package pers.east.learning.algorithm.sort;
 
 /**
- *  插入排序：
+ * 插入排序：
+ * @author eastFu
  */
 public class InsertionSort {
+
+    public static void insertion(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int maxIndex = i;
+            for (int j = i; j > 0; j--) {
+                if (array[j-1] > array[maxIndex]) {
+                    maxIndex=j-1;
+                }
+            }
+            swap(array,i,maxIndex);
+        }
+    }
+
+    public static void swap(int[] ar, int aIndex, int bIndex) {
+        int temp = ar[aIndex];
+        ar[aIndex] = ar[bIndex];
+        ar[bIndex] = temp;
+    }
 }
