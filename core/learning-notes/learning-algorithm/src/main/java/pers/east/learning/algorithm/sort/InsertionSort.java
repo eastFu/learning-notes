d700a7759f142d297844d5fac2c2fc4de0f6e980
eastFu <492510931@qq.com>
2019-09-01 20:50:24 +0800

    algorithm： insertion sort

--- core/learning-notes/learning-algorithm/src/main/java/pers/east/learning/algorithm/sort/InsertionSort.java
+++ core/learning-notes/learning-algorithm/src/main/java/pers/east/learning/algorithm/sort/InsertionSort.java
@@ -1,20 +1,22 @@
 package pers.east.learning.algorithm.sort;
 
+import com.alibaba.fastjson.JSON;
+
 /**
- * 插入排序：
+ * 插入排序： 双层循环，每次找出最大的一个放入每次循环的起始位置
  * @author eastFu
  */
 public class InsertionSort {
 
     public static void insertion(int[] array) {
         for (int i = 1; i < array.length; i++) {
-            int maxIndex = i;
             for (int j = i; j > 0; j--) {
-                if (array[j-1] > array[maxIndex]) {
-                    maxIndex=j-1;
+                if (array[j] < array[j-1]) {
+                    swap(array,j,j-1);
+                }else{
+                    break;
                 }
             }
-            swap(array,i,maxIndex);
         }
     }
 
@@ -22,5 +24,11 @@ public class InsertionSort {
         int temp = ar[aIndex];
         ar[aIndex] = ar[bIndex];
         ar[bIndex] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 6, 1, 3, 2, 7, 9, 8};
+        insertion(arr);
+        System.out.println(JSON.toJSONString(arr));
     }
 }
