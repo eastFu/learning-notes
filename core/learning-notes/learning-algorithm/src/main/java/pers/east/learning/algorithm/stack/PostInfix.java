cf44a687744a216dd58354cd8ae228984d35640c
eastFu <492510931@qq.com>
2019-09-05 09:06:46 +0800

    algorithm： stack 后缀表达式

---
+++ core/learning-notes/learning-algorithm/src/main/java/pers/east/learning/algorithm/stack/PostInfix.java
@@ -0,0 +1,20 @@
+package pers.east.learning.algorithm.stack;
+
+/**
+ * 用栈来实现： 把中缀表达式转换成为后缀表达式
+ * @author eastFu
+ */
+public class PostInfix {
+
+
+    /**
+     * 把中缀表达式转化为后缀表达式
+     * @param str 要转换的中缀表达式
+     * @return 转换成的后缀表达式
+     */
+    public String doTransfer(String str){
+
+
+        return null;
+    }
+}
