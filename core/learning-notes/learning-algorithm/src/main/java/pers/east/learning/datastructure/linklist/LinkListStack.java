8ed8e5b6c22acd2797a58e2087abce828b87756e
eastFu <492510931@qq.com>
2019-09-27 20:38:22 +0800

    algorithm：recursive

---
+++ core/learning-notes/learning-algorithm/src/main/java/pers/east/learning/datastructure/linklist/LinkListStack.java
@@ -0,0 +1,51 @@
+package pers.east.learning.datastructure.linklist;
+
+/**
+ * 链表练习： 使用链表来实现栈
+ *
+ * @author Administrator
+ */
+public class LinkListStack {
+
+    private SingleLinkList theList = new SingleLinkList();
+
+    public boolean push(int id) {
+        theList.insertFirst(id);
+        return true;
+    }
+
+    public int pop() {
+        return theList.removeFirst().getId();
+    }
+
+    public int peek() {
+        return theList.peekFirst().getId();
+    }
+
+    public boolean isEmpty() {
+        return theList.isEmpty();
+    }
+
+    public void print(){
+        theList.displayList();
+    }
+
+    public static void main(String[] args) {
+        LinkListStack myStack = new LinkListStack();
+        myStack.push(2);
+        myStack.push(7);
+        myStack.push(6);
+        myStack.push(5);
+        myStack.push(3);
+
+        myStack.print();
+
+        System.out.println("push result : " + myStack.push(31));
+        System.out.println(myStack.pop());
+        myStack.print();
+        System.out.println(myStack.peek());
+        System.out.println(myStack.isEmpty());
+        myStack.print();
+
+    }
+}
