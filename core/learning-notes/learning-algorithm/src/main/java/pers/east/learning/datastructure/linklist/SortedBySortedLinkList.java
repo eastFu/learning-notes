73802db4cca971ce9dfe4413c4050e5ff82dfdf2
eastFu <492510931@qq.com>
2019-09-27 20:38:46 +0800

    algorithm：recursive

---
+++ core/learning-notes/learning-algorithm/src/main/java/pers/east/learning/datastructure/linklist/SortedBySortedLinkList.java
@@ -0,0 +1,29 @@
+package pers.east.learning.datastructure.linklist;
+
+import java.util.Arrays;
+
+/**
+ * 链表练习： 使用有序链表来实现排序
+ * @author Administrator
+ */
+public class SortedBySortedLinkList {
+
+    public static int[] sort(int[] arr){
+        int[] ret = new int[arr.length];
+        SortedLinkList sortedLinkList = new SortedLinkList();
+        for(int i:arr){
+            sortedLinkList.insertFirst(i);
+        }
+        int index =0;
+        while (!sortedLinkList.isEmpty()){
+            ret[index] = sortedLinkList.removeFirst().getId();
+            index++;
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {6,7,3,4,1,8,9};
+        Arrays.stream(SortedBySortedLinkList.sort(arr)).forEach(System.out::println);
+    }
+}
---
+++ core/learning-notes/learning-algorithm/src/main/java/pers/east/learning/datastructure/linklist/SortedLinkList.java
@@ -0,0 +1,99 @@
+package pers.east.learning.datastructure.linklist;
+
+/**
+ * 链表练习： 有序链表的实现
+ * @author Administrator
+ */
+public class SortedLinkList {
+
+    private LinkNode firstNode;
+
+    public void insertFirst(int id){
+        LinkNode newLink = new LinkNode(id);
+        LinkNode previous = null;
+        LinkNode current = firstNode;
+        while(current!=null&&id>current.getId()){
+            previous = current;
+            current = current.getNext();
+        }
+        if(previous==null){
+            firstNode = newLink;
+        }else {
+            previous.setNext(newLink);
+        }
+        newLink.setNext(current);
+    }
+
+    public LinkNode removeFirst(){
+        LinkNode temp = firstNode;
+        firstNode = temp.getNext();
+        return temp;
+    }
+
+    public LinkNode peekFirst(){
+        return firstNode;
+    }
+
+    public boolean isEmpty(){
+        return firstNode==null;
+    }
+
+    public LinkNode find(int id){
+        LinkNode node = firstNode;
+        while (node.getId()!=id){
+            if(node.getNext()==null){
+                return null;
+            } else {
+                node = node.getNext();
+            }
+        }
+        return node;
+    }
+
+    public LinkNode remove(int id){
+        LinkNode del = firstNode;
+        LinkNode preNode = firstNode;
+        while (del.getId()!=id){
+            if (del.getNext()==null){
+                return null;
+            } else {
+                preNode = del;
+                del = del.getNext();
+            }
+        }
+
+        if (del.equals(firstNode)){
+            firstNode=preNode;
+        } else {
+            preNode.setNext(del.getNext());
+        }
+        return del;
+    }
+
+    public void displayList(){
+        System.out.println("============link list============");
+        LinkNode template = firstNode;
+        while (template!=null){
+            template.print();
+            template =template.getNext();
+        }
+        System.out.println("============link list============");
+    }
+
+    public static void main(String[] args) {
+        SortedLinkList single = new SortedLinkList();
+        single.insertFirst(2);
+        single.insertFirst(4);
+        single.insertFirst(3);
+        single.insertFirst(1);
+        single.displayList();
+        single.removeFirst();
+        single.displayList();
+
+        single.remove(4);
+        single.displayList();
+
+        single.insertFirst(4);
+        single.displayList();
+    }
+}
