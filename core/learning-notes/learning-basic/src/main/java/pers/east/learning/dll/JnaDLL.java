5fd8a179f5928ee3bf77113e00dacd43f0eaef03
eastFu <492510931@qq.com>
2019-11-22 11:31:19 +0800

    fix

--- core/learning-notes/learning-basic/src/main/java/pers/east/learning/dll/JnaDLL.java
+++ core/learning-notes/learning-basic/src/main/java/pers/east/learning/dll/JnaDLL.java
@@ -16,7 +16,6 @@ public class JnaDLL {
     public interface CLibrary extends Library {
         // DLL文件默认路径为项目根目录，若DLL文件存放在项目外，请使用绝对路径。（此处：(Platform.isWindows()?"msvcrt":"c")指本地动态库msvcrt.dll）
         CLibrary INSTANCE = (CLibrary) Native.loadLibrary((Platform.isWindows() ? "msvcrt" : "c"), CLibrary.class);
-
         // 声明将要调用的DLL中的方法,可以是多个方法(此处示例调用本地动态库msvcrt.dll中的printf()方法)
         void printf(String format, Object... args);
     }
