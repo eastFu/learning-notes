661fe2cdcdf4578e3bb4732dd541a018025b1a99
eastFu <492510931@qq.com>
2020-02-16 00:45:27 +0800

    fix

--- core/learning-notes/learning-basic/src/main/java/pers/east/learning/spi/Chinese.java
+++ core/learning-notes/learning-basic/src/main/java/pers/east/learning/spi/Chinese.java
@@ -7,7 +7,6 @@ package pers.east.learning.spi;
  * @date 2019/5/29 19:40
  */
 public class Chinese implements People{
-    
     @Override
     public void say() {
         System.out.println("chinese");
