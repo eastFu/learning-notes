642398e268dcd4a1cb7105880af4f1b0a1538cc1
eastFu <492510931@qq.com>
2020-05-07 14:57:50 +0800

    fix

---
+++ core/learning-notes/learning-algorithm/src/main/java/pers/east/learning/algorithm/jayspt/Demo.java
@@ -0,0 +1,19 @@
+package pers.east.learning.algorithm.jayspt;
+
+import org.jasypt.util.text.BasicTextEncryptor;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        //设置加密密钥
+        textEncryptor.setPassword("MySalt");
+        //加密信息
+        String encryptedText = textEncryptor.encrypt("This is a secret message.");
+        System.out.println("encryptedText:" + encryptedText);
+        //解密
+        String decryptedText = textEncryptor.decrypt(encryptedText);
+        System.out.println("decryptedText:" + decryptedText);
+    }
+
+}
