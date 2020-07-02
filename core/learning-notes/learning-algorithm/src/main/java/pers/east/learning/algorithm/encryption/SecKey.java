775e027e344a40027fc998efd00b31acbd82dd92
eastFu <492510931@qq.com>
2020-07-02 18:57:32 +0800

    fix

--- core/learning-notes/learning-algorithm/src/main/java/pers/east/learning/algorithm/encryption/SecKey.java
+++ core/learning-notes/learning-algorithm/src/main/java/pers/east/learning/algorithm/encryption/SecKey.java
@@ -2,16 +2,15 @@ package pers.east.learning.algorithm.encryption;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
+import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
 import javax.crypto.spec.DESedeKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
-import java.security.Provider;
+import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.util.Arrays;
@@ -34,7 +33,6 @@ public class SecKey {
         return key;
     }
 
-
     public static byte[] hex(String keyStr) throws UnsupportedEncodingException {
         byte[] key = new byte[24];    //声明一个24位的字节数组，默认里面都是0
         byte[] temp = keyStr.getBytes("gbk");    //将字符串转成字节数组
@@ -49,8 +47,86 @@ public class SecKey {
         return key;
     }
 
+    /*
+     * 密钥的长度: DES算法，则密钥长度必须是56位； DESede，则可以是112或168位，其中112位有效； AES，可以是128,
+     * 192或256位； Blowfish，则可以是32至448之间可以被8整除的数； HmacMD5和HmacSHA1默认的密钥长度都是64个字节。
+     */
+    public static byte[] getKey(String password) throws NoSuchAlgorithmException {
+        KeyGenerator kgen = KeyGenerator.getInstance("DESede");// 创建AES的Key生产者
+
+//        byte[] temp=new byte[]{48,19,112,87,191-256,153-256,244-256,239-256};
+//        byte[] temp=new byte[]{1, 35, 69, 103, 137-256, 171-256, 205-256, 239-256};
+//        kgen.init(168, new SecureRandom(temp));// 利用用户密码作为随机数初始化出
+        kgen.init(112, new SecureRandom(password.getBytes()));
+        // 128位的key生产者
+        //加密没关系，SecureRandom是生成安全随机数序列，password.getBytes()是种子，只要种子相同，序列就一样，所以解密只要有password就行
+
+        SecretKey secretKey = kgen.generateKey();// 根据用户密码，生成一个密钥
+//        System.out.println(kgen.getAlgorithm());
+//        System.out.println(kgen.getProvider());
+
+        byte[] enCodeFormat = secretKey.getEncoded();// 返回基本编码格式的密钥，如果此密钥不支持编码，则返回null。
+        return enCodeFormat;
+    }
+
+
+    public static void get3DesKey(byte[] key) throws InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException {
+        DESedeKeySpec spec = new DESedeKeySpec(key);
+        SecretKeyFactory keyfactory = SecretKeyFactory.getInstance("DES");
+        Key deskey = keyfactory.generateSecret(spec);
+        System.out.println(deskey.getEncoded());
+    }
+
+
+    private static byte[] encrypt(byte[] data, byte[] key) throws Exception {
+        // 生成一个可信任的随机数源
+        SecureRandom sr = new SecureRandom();
+        // 从原始密钥数据创建DESKeySpec对象
+        DESKeySpec dks = new DESKeySpec(key);
+        // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+        SecretKey securekey = keyFactory.generateSecret(dks);
+
+        // Cipher对象实际完成加密操作
+        Cipher cipher = Cipher.getInstance("DES");
+        // 用密钥初始化Cipher对象
+        cipher.init(Cipher.ENCRYPT_MODE, securekey, sr);
+        return cipher.doFinal(data);
+    }
+
+
+    public static void Cripto(String Password)
+    {
+        String PasswordCripto = "";
+        try
+        {
+            String encryptionKey = "anyEncryptionString";
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.update(encryptionKey.getBytes("UTF-8"), 0, encryptionKey.length());
+            byte[] encryptionKeyBytes = messageDigest.digest();
+
+            SecretKeySpec Key = new SecretKeySpec(encryptionKeyBytes,"DESede");
+            Cipher cipher = Cipher.getInstance("DESEDE/ECB/NoPadding");
+            cipher.init(Cipher.ENCRYPT_MODE, Key);
+            byte[] encryptedBytes = cipher.doFinal("12341234".getBytes("UTF-8"));
+
+            System.out.println(Arrays.toString(encryptedBytes));
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws Exception{
-        System.out.println(Arrays.toString(key1("1234567")));
-        get3DESKey();
+//        System.out.println(Arrays.toString(key1("10506290")));
+//        get3DESKey();
+
+//        System.out.println(Arrays.toString("10506290".getBytes("gbk")));
+//        System.out.println(Arrays.toString(getKey("1050629")));
+
+
+//        System.out.println(Arrays.toString(encrypt("12341234".getBytes("gbk"),"10506290".getBytes("gbk"))));
+
+        Cripto("");
     }
 }
---
+++ core/learning-notes/learning-algorithm/src/main/java/pers/east/learning/algorithm/encryption/TestEncryption.java
@@ -0,0 +1,105 @@
+package pers.east.learning.algorithm.encryption;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import javax.crypto.*;
+import javax.crypto.spec.DESedeKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+
+public class TestEncryption {
+
+    /**
+     *  DES AES DESede
+     */
+    private static final String KEY_ALGORITHM = "DES";
+
+    /**
+     * padding type:  NoPadding  PKCS5Padding   PKCS1Padding   ISO10126Padding  OAEPPadding
+     */
+    private static final String DEFAULT_CIPHER_ALGORITHM = "DES/ECB/NoPadding";
+
+
+
+    public static void main(String[] args) throws Exception{
+        getDesKey1("1050629");
+        System.out.println(desEncry("12341234","1050629"));
+    }
+
+    public static String desEncry(String text,String key) throws NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, InvalidKeySpecException, UnsupportedEncodingException {
+        Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
+        byte[] raw=new byte[]{105,214-256,68,92,187-256,159-256,59,199-256};
+//        raw=getDesKey1(key);
+        SecretKeySpec skeySpec = new SecretKeySpec(raw, KEY_ALGORITHM);
+
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+        byte[] encrypted = cipher.doFinal(text.getBytes());
+        return byteToHexString(encrypted);
+    }
+
+    public static String desDecry(String text,String key){
+
+        return "";
+    }
+
+
+    public static byte[] getDesKey1(String key) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, UnsupportedEncodingException {
+        byte[] mode = new byte[8];
+        byte[] temp = key.getBytes("gbk");
+        System.arraycopy(temp, 0, mode, 0, temp.length);
+
+        KeyGenerator gen = KeyGenerator.getInstance("DES");// 创建AES的Key生产者
+
+        gen.init(56, new SecureRandom(mode));
+        // 128位的key生产者
+        //加密没关系，SecureRandom是生成安全随机数序列，password.getBytes()是种子，只要种子相同，序列就一样，所以解密只要有password就行
+
+        SecretKey secretKey = gen.generateKey();// 根据用户密码，生成一个密钥
+
+        byte[] enCodeFormat = secretKey.getEncoded();// 返回基本编码格式的密钥，如果此密钥不支持编码，则返回null。
+        System.out.println(Arrays.toString(enCodeFormat));
+        return enCodeFormat;
+    }
+
+    public static void getDesKey2(String key){
+
+    }
+
+    public static String byteToHexString(byte[] data) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < data.length; ++i) {
+            String ch = Integer.toHexString(data[i] & 0xFF).toUpperCase();
+            if (ch.length() == 2)
+                sb.append(ch);
+            else
+                sb.append("0").append(ch);
+        }
+        return sb.toString();
+    }
+
+    public static byte[] stringTohexByte(String hexString) {
+        if (hexString == null || hexString.equals("")) {
+            return null;
+        }
+        hexString = hexString.toUpperCase();
+        int length = hexString.length() / 2;
+        char[] hexChars = hexString.toCharArray();
+        byte[] d = new byte[length];
+        for (int i = 0; i < length; i++) {
+            int pos = i * 2;
+            d[i] = (byte) (charToByte(hexChars[pos]) << 4 | charToByte(hexChars[pos + 1]));
+        }
+        return d;
+    }
+
+    public static byte charToByte(char c) {
+        return (byte) "0123456789ABCDEF".indexOf(c);
+    }
+}
