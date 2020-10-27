0ac4e5aea7b105918f5b37d7e8414fce1ab13e92
eastFu <492510931@qq.com>
2020-10-27 11:11:56 +0800

    kafka

---
+++ core/learning-notes/learning-kafka/src/main/java/cn/gyyx/learning/kafka/KafkaDemo.java
@@ -0,0 +1,16 @@
+package cn.gyyx.learning.kafka;
+
+public class KafkaDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void test1(){
+
+    }
+
+    public void config(){
+
+    }
+}
