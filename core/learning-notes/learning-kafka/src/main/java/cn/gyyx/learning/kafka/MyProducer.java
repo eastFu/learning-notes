05d1b707b2e8a2c3c51d67f57d849c55415681ce
eastFu <492510931@qq.com>
2020-10-29 14:30:35 +0800

    storm

--- core/learning-notes/learning-kafka/src/main/java/cn/gyyx/learning/kafka/MyProducer.java
+++ core/learning-notes/learning-kafka/src/main/java/cn/gyyx/learning/kafka/MyProducer.java
@@ -37,5 +37,4 @@ public class MyProducer{
         }
         producer.close();
     }
-
 }
--- core/learning-notes/learning-storm/src/main/java/pers/east/learning/storm/Demo.java
+++ core/learning-notes/learning-storm/src/main/java/pers/east/learning/storm/Demo.java
@@ -5,6 +5,11 @@ public class Demo {
     public static void main(String[] args) {
 
 
+
+    }
+
+    public void init(){
+
     }
 
 }
