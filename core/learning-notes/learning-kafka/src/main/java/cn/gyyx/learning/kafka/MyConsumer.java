0845a4cd17086c672ba6a6225f66ef6b3bfdefb6
eastFu <492510931@qq.com>
2020-11-15 12:23:18 +0800

    postgresql

--- core/learning-notes/learning-kafka/src/main/java/cn/gyyx/learning/kafka/MyConsumer.java
+++ core/learning-notes/learning-kafka/src/main/java/cn/gyyx/learning/kafka/MyConsumer.java
@@ -38,6 +38,7 @@ public class MyConsumer{
         this.consumer.subscribe(Collections.singletonList(topic));
     }
 
+
     public void doIt() throws InterruptedException {
         System.out.println("---------开始消费---------");
         while (true) {
